abeae68b0ed071798282fb643ffc86e2c5205815
Seulvagem <ffelipe.dias@gmail.com>
2021-12-17 11:30:50 -0300

    added dynm->json conversion

---
+++ aws-util.js
@@ -0,0 +1,25 @@
+const r = require("ramda")
+const u = require("js-util")
+
+
+const typeToFn = {
+    "S": r.identity,
+    "N": parseInt,
+    "BOOL": r.identity,
+    "NULL": r.always(null),
+    "SS": u.set,
+    "NS": r.compose(u.set, r.map(parseInt)),
+    "L": formatFromDynamo,
+    "M": formatFromDynamo
+}
+
+const dynmToJson = (input) => {
+    return r.map((val) => { //val => {tipo:valor}
+        const [type, value] = Object.entries(val)[0]
+        return typeToFn[type](value)
+    }, (input))
+}
+
+module.exports = {
+    dynmToJson
+}
