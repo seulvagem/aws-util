8397a2d851f0f76101d6528a9de2624a3f0ecfe7
Seulvagem <ffelipe.dias@gmail.com>
2021-12-17 18:18:44 -0300

    added type fn to get dynm type for js element

--- aws-util.js
+++ aws-util.js
@@ -19,6 +19,54 @@ const typeToFn = {
     "M": dynmToJson
 }
 
+// const jsonToDynm = (input) => {
+//     return r.map((val) => { //val => {tipo:valor}
+//         const [type, value] = Object.entries(val)[0]
+//         return typeToFn[type](value)
+//     }, (input))
+// }
+
+// const jsTypeToFn = {
+//     "S": r.identity,
+//     "N": parseInt,
+//     "BOOL": r.identity,
+//     "NULL": r.always(null),
+//     "SS": u.set,
+//     "NS": r.compose(u.set, r.map(parseInt)),
+//     "L": dynmToJson,
+//     "M": dynmToJson
+// }
+
+const typeToDynmType = {
+    "string": "S",
+    "number": "N",
+    "boolean": "BOOL"
+}
+
+const whichSet = (s) => {
+    console.log(s)
+    const first = s.values().next().value
+    const ftype = typeToDynmType[typeof (first)]
+    return ftype + "S"
+}
+
+const isObject = (x) => (x === Object(x))
+
+const whatObject = r.cond([
+    [Array.isArray, r.always("L")],
+    [r.equals(null), r.always("NULL")],
+    [r.is(Set), whichSet],
+    [isObject, r.always("M")],
+    [r.T, (x) => { throw new Error("Invalid type: " + x) }]
+])
+
+const type = (x) => {
+    const typ = typeof (x)
+
+    return (typ === "object") ? whatObject(x) : typeToDynmType[typ]
+}
+
 module.exports = {
-    dynmToJson
+    dynmToJson,
+    type
 }
